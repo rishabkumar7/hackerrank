512c97aafc7d9aae30af4fb3e7788abea7886d21
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2020-02-23 14:35:55 -0500

    Day 5 loops

---
+++ Day5-Loops.js
@@ -0,0 +1,7 @@
+function main() {
+    var n = parseInt(readLine());
+    var i = parseInt(readLine());
+    for (i = 1; i <=10; i++) {
+      console.log(n + ' x ' + i + ' = ' + (n*i));
+    }
+  }
