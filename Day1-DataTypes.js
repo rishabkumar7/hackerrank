485cbec0c83ed8b3359f0322fbd381ed082e1dc1
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2020-02-22 23:17:08 -0500

    added code to 4 days challenges

---
+++ Day1-DataTypes.js
@@ -0,0 +1,16 @@
+
+    // Declare second integer, double, and String variables.
+
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    var i0 = readLine();
+    var d0 = readLine();
+    var s0 = readLine();
+
+    console.log(i + Number(i0) + "\n" + (Number(d0) + d).toFixed(1) + "\n" +s +s0);
